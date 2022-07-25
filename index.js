72f082848ef4536bae27f4f0bf13d8a89d990a7e
arif99050 <arifulislam99050@gmail.com>
2022-07-25 22:18:49 +0600

    implement reducer

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
+const INCREAMENT = "INCREAMENT"
+const DECREAMENT = "DECREAMENT"
+
 // declare state 
-const initaialState=()=>{
-    count:0;
-}
-const INCREAMENT="INCREAMENT"
+const initaialState = () => {
+    count: 0
+};
+
 //  action 
-const action=()=>{
-    return{
-        type:"INCREAMENT",
+const incAction = () => {
+    return {
+        type: "INCREAMENT",
     }
 
 }
+const drcAction = () => {
+    return {
+        type: "DECREAMENT",
+    }
+
+}
+// Reducer 
+const counter = (state = initaialState, action) => {
+    switch (action.type) {
+        case "INCREAMENT":
+            return {
+                ...state,
+            count : state.count + 1,
+
+            };
+        case "DECREAMENT":
+            return {
+                ...state,
+            count : state.count -1 1,
+
+            };
+        default:
+            state;
+    }
+
+}
+
